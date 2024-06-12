e81a880df6993004d911a10601feb0ef611335b2
dev-mhshakil <mehedihshakil22@gmail.com>
2024-06-12 19:35:47 +0600

    features section

---
+++ src/components/home/Features.jsx
@@ -0,0 +1,57 @@
+import { IoPricetagsOutline } from "react-icons/io5";
+import { MdOutlineManageAccounts, MdPeopleOutline } from "react-icons/md";
+import { PiVideo } from "react-icons/pi";
+
+const Features = () => {
+  const featuresData = [
+    {
+      icon: <MdOutlineManageAccounts size={60} />,
+      title: "Exclusive Advisor",
+      description: "Expert guidance for personalized learning",
+    },
+    {
+      icon: <PiVideo size={60} />,
+      title: "Video Tutorial",
+      description: "Engaging tutorials for effective learning",
+    },
+    {
+      icon: <IoPricetagsOutline size={60} />,
+      title: "Affordable Pricing",
+      description: "Affordable pricing for personalized learning",
+    },
+    {
+      icon: <MdPeopleOutline size={60} />,
+      title: "Community Support",
+      description: "Expert guidance for personalized learning",
+    },
+  ];
+  return (
+    <div className="max-w-[1240px] mx-auto px-4">
+      <p className="text-center text-primary text-xl">Features</p>
+      <h1 className="text-4xl font-semibold py-4 text-center">
+        One Platform Many Course
+      </h1>
+
+      <div className="flex flex-col md:flex-row gap-4 mt-2 md:mt-8">
+        {featuresData.map((feature, index) => {
+          return (
+            <div
+              key={index}
+              className="flex flex-col justify-center items-center w-[360px] h-[340px] mx-auto border border-opacity-50 border-secondary/60 hover:rounded-tr-[60px] ease-in duration-300 rounded-md  shadow-sm hover:border-2  hover:rounded-r-lg hover:border-primary hover:bg-secondary/60"
+            >
+              <div className="bg-secondary hover:text-white text-primary hover:bg-primary p-4 rounded-full">
+                {feature.icon}
+              </div>
+              <div className="mt-4 text-xl font-semibold">{feature.title}</div>
+              <div className="text-center text-black/60 mt-6">
+                {feature.description}
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Features;
--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -2,32 +2,38 @@ import { BsMouse } from "react-icons/bs";
 
 const Hero = () => {
   return (
-    <div className="max-w-[1240px] h-[calc(100vh-96px)] overflow-auto mx-auto px-2 md:px-4">
-      <div className="flex flex-col md:flex-row justify-center items-center">
-        <div className="w-full md:w-[40%]">
-          <h1 className="text-3xl md:text-4xl lg:text-6xl text-center md:text-left font-bold pb-2 md:pb-4">
-            Empower Your Learning <span className="text-primary">Journey</span>
-          </h1>
-          <p className="text-base md:text-md lg:text-xl text-center md:text-left px-2 py-3 md:py-0 md:px-0">
-            Discover, Learn, and Achieve with Our Comprehensive LMS Platform.
-            Join a Community of Learners and Unlock Your Full Potential Today!
-          </p>
-          <div className="flex justify-center md:justify-start gap-2 mt-2 md:mt-4 lg:mt-6">
-            <button className="py-3 px-4 rounded-md bg-secondary ">
-              All Courses
-            </button>
-            <button className="py-3 px-4 rounded-md bg-primary text-white">
-              Contact Us
-            </button>
-          </div>
+    <div className=" h-[calc(100vh-96px)] ">
+      <div className="max-w-[1240px] mx-auto px-2 md:px-4">
+        <div className="flex flex-col md:flex-row justify-center items-center">
+          <div className="w-full md:w-[40%]">
+            <p className="text-xl font-semibold mb-4">
+              Online E-learning Courses
+            </p>
+            <h1 className="text-3xl md:text-4xl lg:text-6xl text-center md:text-left font-bold pb-2 md:pb-4">
+              Empower Your Learning{" "}
+              <span className="text-primary">Journey</span>
+            </h1>
+            <p className="text-base md:text-md text-center md:text-left px-2 py-3 md:py-0 md:px-0">
+              Discover, Learn, and Achieve with Our Comprehensive LMS Platform.
+              Join a Community of Learners and Unlock Your Full Potential Today!
+            </p>
+            <div className="flex justify-center md:justify-start gap-2 mt-2 md:mt-4 lg:mt-6">
+              <button className="py-3 px-4 rounded-md bg-secondary ">
+                All Courses
+              </button>
+              <button className="py-3 px-4 rounded-md bg-primary text-white">
+                Contact Us
+              </button>
+            </div>
 
-          <div className="flex justify-center md:justify-start items-center gap-4 mt-8">
-            <BsMouse size={20} />
-            <p>Scroll to see more sections</p>
+            <div className="flex justify-center md:justify-start items-center gap-4 mt-8">
+              <BsMouse size={20} />
+              <p>Scroll to see more sections</p>
+            </div>
           </div>
-        </div>
-        <div className="w-full md:w-[60%] order-first md:order-last">
-          <img src="src/assets/hero.png" alt="hero" className="" />
+          <div className="w-full md:w-[60%] order-first md:order-last">
+            <img src="src/assets/hero.png" alt="hero" className="" />
+          </div>
         </div>
       </div>
     </div>
